5823271ac00557ccc1e5b55c600eae2dfaaba77e
Kozo Nishida <kozo.nishida@gmail.com>
2019-12-12 00:49:10 +0900

    Create install.R

---
+++ binder/install.R
@@ -0,0 +1,4 @@
+install.packages(c("BiocManager", "remotes"), repos="http://cran.r-project.org")
+BiocManager::install(c("SingleCellExperiment", "LRBase.Hsa.eg.db", "MeSH.Hsa.eg.db", "GSEABase", "msigdbr"), suppressUpdates=TRUE)
+remotes::install_github("rikenbit/scTensor")
+remotes::install_github("rikenbit/scTGIF")
