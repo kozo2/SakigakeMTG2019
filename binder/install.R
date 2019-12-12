b1009433e8a0751446b8c5f09d00cc761ac115c3
Kozo Nishida <kozo.nishida@gmail.com>
2019-12-13 07:49:28 +0900

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -1,2 +1,3 @@
-install.packages(c("BiocManager", "remotes"), repos="http://cran.r-project.org")
+install.packages(c("BiocManager"), repos="http://cran.r-project.org")
+BiocManager::install(version="3.10")
 BiocManager::install(c("SingleCellExperiment", "LRBase.Hsa.eg.db", "MeSH.Hsa.eg.db", "GSEABase", "msigdbr", "scTensor", "scTGIF"), suppressUpdates=TRUE)
